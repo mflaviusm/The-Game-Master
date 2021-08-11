6f6cc637ee98933bebcc685455403f0c8b552cd9
mflaviusm <you@example.com>
2021-08-11 14:11:31 +0100

    create javascript file and define all required elements

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//getting all required elements
+
+const startButton = document.querySelector('.start-btn');
+const rulesBox = document.querySelector('.rules-box');
+const quitButton = document.querySelector('.buttons .quit');
+const continueButton = document.querySelector('.buttons .continue');
+const gameBox = document.querySelector('.game-box');
+const optionsBox = document.querySelector('.options-box');
+const timeLeft = gameBox.querySelector('.timer .time-left');
+const nextButton = document.querySelector('.next-btn');
+const exitButton = document.querySelector('.quit-btn');
+const resultsBox = document.querySelector('.results');
+const restartButton = resultsBox.querySelector('.buttons .restart');
+const returnButton = resultsBox.querySelector('.buttons .quit');
+let plusOne = document.querySelector('.total-q #correct-q');
+let minusOne = document.querySelector('.total-q #wrong-q');
